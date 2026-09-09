4af8ce700bdad978a5848bca01f303ac3799d49f
NnDIz <nndiz020509@gmail.com>
2026-09-09 12:53:13 +0300

    Add IndexedDB database for tracks

--- script.js
+++ script.js
@@ -18,3 +18,45 @@ console.log(recentTracks);
 const trackCard = document.createElement("div");
 trackCard.classList.add("track-card");
 recentTracks.appendChild(trackCard);
+
+const request = indexedDB.open("SoundWaveDB", 2);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+
+    if (!db.objectStoreNames.contains("tracks")) {
+            db.createObjectStore("tracks", 
+            { 
+                keyPath: "id", 
+                autoIncrement: true 
+            }
+        );
+    }
+    
+};
+
+request.onsuccess = function (event) {
+    const db = event.target.result;
+    console.log("Database opened successfully");
+
+    const transation = db.transaction("tracks", "readonly");
+    const store = transation.objectStore("tracks");
+
+    const getAllRequest = store.getAll();
+
+    getAllRequest.onsuccess = function (event) {
+        const tracksFromDB = getAllRequest.result;
+
+        console.log("Tracks retrieved successfully");
+        console.log(tracksFromDB);
+    };
+
+    // const transaction = db.transaction("tracks", "readwrite");
+    // const store = transaction.objectStore("tracks");
+
+
+};
+
+request.onerror = function (event) {
+    console.error("Database error: " + event.target.errorCode);
+};
